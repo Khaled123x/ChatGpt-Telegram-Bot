4a769b6c9e8d12ecc9524f64d13f9f3c74a78237
Khaled123x <143716155+Khaled123x@users.noreply.github.com>
2023-08-31 20:50:34 +0600

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN = bot token from 6403982406:AAGxSVk9p1-zKc3rm4wvJ0qo4-dXrVSPyKE
-API_ID = api id from 20110964
-API_HASH = api hash from 5f4a53387dff48bf9c4b4bab97491ad7
+BOT_TOKEN = bot token from t.me/botfather
+API_ID = api id from telegram.org
+API_HASH = api hash from telegram.org
     
